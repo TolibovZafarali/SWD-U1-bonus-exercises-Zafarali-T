1ea37dbb62cd82fe98c628eaa071afc9f05417d1
Zafarali Tolibov <tolibovzafarali7@gmail.com>
2025-04-21 15:49:10 -0500

    Completed practices

--- objects/01-object-basics.js
+++ objects/01-object-basics.js
@@ -21,7 +21,13 @@ let car1 = {
 }
 
 // TRY IT: Create an object representing your own car (past, present, or future!)
-
+let car2 = {
+	year: 2019,
+	make: "Hyundai",
+	model: "Elantra",
+	style: "Sedan",
+	color: "black"
+}
 
 
 /**** ACCESSING PROPERTIES OF OBJECTS ****/
@@ -51,11 +57,10 @@ console.log(`In 1999, my dad gave me his old drill. It was a ${oldDrill["brand"]
 console.log(`After 20 years I finally got a new drill. It's a ${newDrill.brand}.`);
 
 // TRY IT! Use bracket notation to print whether oldDrill is cordless.
-
+console.log("Cordless: ", oldDrill["cordless"])
 
 // TRY IT! Use dot notation to print the voltage of newDrill's battery.
-
-
+console.log("Battery: ", newDrill.battery)
 
 /**** ADDING & MODIFYING OBJECT PROPERTIES ****/
 
@@ -80,6 +85,9 @@ city.metroPopulation = 2807338;
 console.log(city);
 
 // TRY IT! Use bracket notation to change the name of the state so that it is spelled out instead of being a two-letter abbreviation. Print the resulting object.
-
+city["state"] = "Missouri"
+console.log(city)
 
 // TRY IT! Use dot notation to add a new property called "claimToFame", with the value being the Gateway Arch, of course! Print the resulting object.
+city.claimToFame = "Gateway Arch"
+console.log(city)
--- objects/02-object-ops.js
+++ objects/02-object-ops.js
@@ -86,7 +86,25 @@ for (let restaurant of restaurants) {
 	Create two objects representing different local attractions (zoo, gardens, museums, etc), each with the following properties: name, location, adultTicketPrice, childTicketPrice, and famousFeatures (array of three strings). Be creative! Place the objects in an array.
 */
 
+let zoo = {
+	name: "Saint Louis Zoo",
+	location: "1 Government Dr, St. Louis, MO 63110",
+	adultTicketPrice: 50,
+	childTicketPrice: 0,
+	famousFeatures: ["Aquariums", "Trains", "Educational programs"]
+}
+
+let garden = {
+	name: "Missouri Botanical Garden",
+	location: "4344 Shaw Blvd, St. Louis, MO 63110",
+	adultTicketPrice: 16,
+	childTicketPrice: 16,
+	famousFeatures: ["Beautiful horticultural displays", "Extensive botanical research", "Dedication to plant conservation"]
+}
+
 // TODO: create objects and put them in an array
+
+let attractions = [zoo, garden];
 
 /*
 	Loop through the array, then within that for loop, use a for...in loop to grab values from each key and print them to the console. If you come across a key storing an array, print each element in the array instead of the array itself. Remember that you need a new index variable with each nested FOR loop (i, j are common).
@@ -94,7 +112,22 @@ for (let restaurant of restaurants) {
 	NOTE: This is a tough one to think through when you're new at this. If you get completely stuck, reference the solution that accompanies this starter code. But make an effort to figure it out yourself first!
 */
 
+for (let index of attractions) {
+	for (let j in index) {
+		if (Array.isArray(index[j])) {
+			for (let arrVal of index[j]) {
+				console.log(`Famous for: ${arrVal}`)
+			}
+		} else {
+			console.log(`${j.toUpperCase()}: ${index[j]}`)
+		}
+	}
+}
+
+
 // TODO: use nested loops to print values inside objects
+
+
 
 /*
 	Last task: loop through the outer array and print a single template literal for each attraction using placeholders referencing the object at each index. Here's an example:
@@ -107,3 +140,9 @@ for (let restaurant of restaurants) {
 
 // TODO: Print a template literal
 
+for (let attraction of attractions) {
+console.log(`\n**********************************************
+${attraction.name} - ${attraction.location}
+**********************************************
+A favorite with the locals, the ${attraction.name} boasts over a dozen features, including the ${attraction.famousFeatures[0].toLowerCase()}, ${attraction.famousFeatures[1].toLowerCase()}, and ${attraction.famousFeatures[2].toLowerCase()}. Admission is $${attraction.adultTicketPrice} for adults, $${attraction.childTicketPrice} for children.`)
+}
